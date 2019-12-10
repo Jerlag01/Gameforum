61c52ab1e84179fd54651d03a9dbac68a7265e72
Lagaet <jerlag_01@outlook.com>
2019-12-10 11:54:48 +0100

    added required connection.php file for DB connection. Don't forget to fill your own database credentials in the proper variables

---
+++ admin/required/connection.php
@@ -0,0 +1,18 @@
+<?php 
+$host = "";
+$user = "";
+$password = "";
+$database = "gameforum";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+$conn->connect_errno;
+
+print $conn->error;
+
+if (mysqli_connect_error()) {
+	echo "Failed to connect to Database: $database" . mysqli_connect_error();
+}
+
+
+?>
